6e2fc2a521ff6cd17772178553cea324f88db136
James Gardner <jamie.gardner25@gmail.com>
2021-10-11 20:47:58 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Input, Button } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonState: 2
+    }
+  }
+
+  changeState(prevState) {
+    this.setState({ buttonState: prevState - 1 });
+  }
+
+  onInputChange(event) {
+    if (event.target.value === 'hello world') {
+      this.setState({ buttonState: 1 })
+    } else if (event.target.value === 'hello') {
+      this.setState({ buttonState: 0 })
+    }
+  }
+
+  render() {
+    if (this.state.buttonState === 2) {
+      var button = <Button loading>Loading</Button>
+    } else if (this.state.buttonState === 0) {
+      button = <Button negative>Not Available</Button>
+    } else if (this.state.buttonState === 1) {
+      button = <Button positive>Create</Button>
+    }
+    return (
+      <React.Fragment>
+        <Input style={{ margin: '10px' }} action={button} onChange={(event) => this.onInputChange(event)} />
+
+        <Button onClick={() => this.changeState(this.state.buttonState)}>state change</Button>
+        <Button onClick={() => this.setState({ buttonState: 2 })}>state reset</Button>
+        <p>buttonState: {this.state.buttonState}</p>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
