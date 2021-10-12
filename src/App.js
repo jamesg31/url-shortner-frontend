f98dd5a203da8cb68874f465621b313afdf510cc
James Gardner <jamie.gardner25@gmail.com>
2021-10-11 22:26:30 -0700

    Initial API Implementation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import { Input, Button } from 'semantic-ui-react';
+
+const api = 'http://localhost:3001/api/'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      buttonState: 2
+      buttonState: 0,
+      urlInput: '',
+      destinationInput: '',
     }
-  }
 
-  changeState(prevState) {
-    this.setState({ buttonState: prevState - 1 });
+    this.timeout = 0;
   }
 
   onInputChange(event) {
-    if (event.target.value === 'hello world') {
-      this.setState({ buttonState: 1 })
-    } else if (event.target.value === 'hello') {
-      this.setState({ buttonState: 0 })
-    }
+    this.setState({ buttonState: 0, urlInput: event.target.value });
+    if (this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      if (event.target.value.length > 0) {
+        axios.get(api + 'urls/' + event.target.value).then((res) => {
+          console.log(res.data);
+          if (res.data.length === 0) {
+            this.setState({ buttonState: 2 });
+          } else {
+            this.setState({ buttonState: 1 });
+          }
+        })
+      }
+    }, 300);
+  }
+
+  onButtonClick(event) {
+    axios.post(api + 'urls', { url: this.state.urlInput, destination: this.state.destinationInput }).then((res) => {
+      console.log(res);
+    })
   }
 
   render() {
-    if (this.state.buttonState === 2) {
+    if (this.state.buttonState === 0) {
       var button = <Button loading>Loading</Button>
-    } else if (this.state.buttonState === 0) {
-      button = <Button negative>Not Available</Button>
     } else if (this.state.buttonState === 1) {
-      button = <Button positive>Create</Button>
+      button = <Button negative>Not Available</Button>
+    } else if (this.state.buttonState === 2) {
+      button = <Button positive onClick={(event) => this.onButtonClick()}>Create</Button>
     }
     return (
       <React.Fragment>
-        <Input style={{ margin: '10px' }} action={button} onChange={(event) => this.onInputChange(event)} />
-
-        <Button onClick={() => this.changeState(this.state.buttonState)}>state change</Button>
-        <Button onClick={() => this.setState({ buttonState: 2 })}>state reset</Button>
-        <p>buttonState: {this.state.buttonState}</p>
+        <Input value={this.state.destinationInput} style={{ margin: '10px' }} placeholder='https://example.com' onChange={(event) => this.setState({ destinationInput: event.target.value })} />
+        <br />
+        <Input value={this.state.urlInput} style={{ marginLeft: '10px' }} action={button} onChange={(event) => this.onInputChange(event)} label={window.location.origin + '/'} placeholder='shortened-url' />
       </React.Fragment>
     );
   }
